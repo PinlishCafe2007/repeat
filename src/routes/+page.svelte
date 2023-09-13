68455a1b5dc00f577ebe922d5cb7dc84bfc3e55b
Nikita <balduev07@gmail.com>
2023-09-13 18:36:56 +0700

    add full calc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,34 @@
 <nav>
-    <p class="name">CALCULATOR</p>
     <div class="calc">
-
+        <div class="inpt1"><input class="number1" type="text"></div>
+        <div class="inpt2"><input class="number2" type="text"></div>
+        <div class="btn">
+            <button class="btn1">+</button>
+            <button class="btn2">-</button>
+        </div>
+        <p class="answer">Ответ:</p>
     </div>
 </nav>
 
 <style>
     .calc{
         border: 2px solid black;
-        width: 20%;
+        width: 100px;
         height: 100px;
-        margin-left: 38%;
+        margin: 10% 45% 0 45%;
     }
-    .name{
-        margin-left: 44%;
-        margin-top: 10%;
+    .number1 {
+        width: 90px;
+        margin: 0 1px 0 1px;
+    }
+    .number2 {
+        width: 90px;
+        margin: 6px 1px 6px 1px;
+    }
+    .btn {
+        margin: 0 25px 0 25px;
+    }
+    .answer {
+        margin: 5px 0 0 0;
     }
 </style>
